7774e2126f8fe8cf85280cb8002929187020de31
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-12-09 14:59:47 +0100

    add team card

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// In allegato trovate un layout di base e
+//  un file js in cui è definito un array di oggetti 
+//  che rappresentano i membro del team.
+// Ogni membro ha le informazioni necessarie per
+//  stampare la relativa card: Nome, Ruolo e Foto.
+// Prendendo come riferimento la card di 
+// esempio presente nell’html, stampiamo
+//  dinamicamente una card per ogni membro del team.
+
 const team = [
   {
     name: 'Wayne Barnett',
@@ -30,3 +39,59 @@ const team = [
     image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+//richiamo il container delle card 
+
+let container_card = document.querySelector('.team-container');
+
+console.log(container_card);
+
+//creo 5 div "team card" da inserire nel container card
+  for (let key in team) {
+
+    //creo i div che contengono le team card
+    let team_card = document.createElement("div");
+
+    //creo i div che contengono le immagini del team
+    let card_image = document.createElement("div");
+
+    // creo gli elementi "immagini"
+    let img = document.createElement("img");
+
+    //creo il div card card text 
+    let card_text = document.createElement("div");
+
+    //creo il source delle img
+    img.src = team[key].image;
+
+    //metto le team card all'interno del container
+    container_card.append(team_card);
+
+    //metto le card image all'interno delle team card
+    team_card.append(card_image);
+
+    //inserisco le immagini all'interno dei div card image
+    card_image.append(img);
+
+    //inserisco i div card text nelle card
+    team_card.append(card_text);
+
+    //inserisco titolo e paragrafo all'interno di card text
+    card_text.append(team[key].name);
+    card_text.append(team[key].role);
+
+    //aggiungo la classe team card ai div team card
+    team_card.classList.add("team-card");
+
+    //aggiungo la classe card image ai div card image
+    card_image.classList.add("card-image");
+
+    //aggiungo la classe card text ai div card text
+    card_text.classList.add("card-text");
+   
+    console.log(team_card);
+  }
+
+
+
+
